8a69bc63db37e94ec9fbff5ddf77abc6b392c877
Zulhisham, Izzat Azfar <izzat.azfar.zulhisham@intel.com>
2021-07-05 20:16:04 -0700

    Add ipu_thm.py

---
+++ ipu_thm.py
@@ -0,0 +1,78 @@
+
+import os, re, sys, time, argparse, subprocess, time
+from invoke import UnexpectedExit
+import paramiko
+
+
+script_name = str(sys.argv[0])
+parser = argparse.ArgumentParser(prog=script_name, description=usage)
+parser.add_argument('-sut_ip', help='SUT IP')
+parser.add_argument('-thm_ip', help='THM IP')
+parser.add_argument('-op', help='Option = [playback, driver_check]')
+
+args = parser.parse_args()
+
+
+target_user = 'root'
+target_pass = ''
+
+if args.sut_ip is not None:
+    sut_ip = args.sut_ip
+else:
+    print ("-sut_ip missing")
+    sys.exit(1)
+
+if args.op is not None:
+     option = args.op
+else:
+     print ("-op missing")
+     sys.exit(1)
+
+sut = paramiko.SSHClient()
+sut.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+sut.connect(hostname=sut_ip,port=22,username=target_user,password=target_pass,allow_agent=False,look_for_keys=False)
+# proc = subprocess.Popen(["prog", "arg"], stdout=subprocess.PIPE)
+host,port = sut_ip,22
+# transport = paramiko.Transport((host,port))
+# username,password = "root",""
+# transport.connect(None,username,password)
+# sftp = paramiko.SFTPClient.from_transport(transport)
+
+def main():
+
+    if option == "ipu_driver_checker":
+        stdin, stdout, stderr = sut.exec_command("python3 /home/root/ipu_checker.py -c ipu_driver_checker")
+        print ("\nSUT executing " + option + " command...\n")
+        time.sleep(3)
+        output = stdout.readlines()
+        sut.close()
+        print ("\nSUT output:\n")
+        print (output)
+        sys.exit(0)
+
+    elif option == "ipu_modules_checker":
+        stdin, stdout, stderr = sut.exec_command("python3 /home/root/ipu_checker.py -c ipu_modules_checker")
+        print ("\nSUT executing " + option + " command...\n")
+        time.sleep(3)
+        output = stdout.readlines()
+        sut.close()
+        print ("\nSUT output:\n")
+        print (output)
+        sys.exit(0)
+		
+    elif option == "playback":
+        stdin, stdout, stderr = sut.exec_command("python3 /home/siv_test_collateral_audio_SUT/siv_val-io-test-apps/audio/new_audio_SUT_checker.py -op " + option + " -drv " +Driver + " -b " + bitdepth + " -s " + rate + " -ch " + channel)
+        execcommand = ("python3 /home/siv_test_collateral_audio_SUT/siv_val-io-test-apps/audio/new_audio_SUT_checker.py -op " + option + " -drv " +Driver + " -b " + bitdepth + " -s " + rate + " -ch " + channel)
+        print (execcommand)
+		
+        #stdin, stdout, stderr = sut.exec_command("python3 /home/siv_test_collateral_audio_SUT/siv_val-io-test-apps/audio/new_audio_SUT_checker.py -op playback -drv HDA -b 32 -s 48 #-ch 2")
+		
+        print ("\nSUT executing " + option + " command...\n")
+        time.sleep(3)
+        output = stdout.readlines()
+        sut.close()
+        print ("\nSUT output:\n")
+        print (output)
+        sys.exit(0)
+
+main()

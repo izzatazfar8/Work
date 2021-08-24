5046022a33da789d7e5972dc2dfb058be5872051
Zulhisham, Izzat Azfar <izzat.azfar.zulhisham@intel.com>
2021-08-24 01:53:46 -0700

    Update ipu_checker.py

--- ipu_checker.py
+++ ipu_checker.py
@@ -35,8 +35,40 @@ def modules_checker():
         else:
           print ("IPU Modules FAILED")  
 	 		 
-	
- 	
+def exposure():
+    print("----- Exposure Checking -----")
+    os.system("v4l2-ctl --all -d /dev/v4l-subdev12 > /home/root/exposure_check.log")
+
+    #verdict = false
+    with open('/home/root/exposure_check.log') as f:
+        if 'exposure 0x00980911 (int)    : min=0 max=2355 step=2 default=2355 value=2355' in f.read():
+          print ("Exposure PASS")
+        else:
+          print ("Exposure FAILED")	
+
+def digital_gain():
+    print("----- Digital Value Gain Checking -----")
+    os.system("v4l2-ctl --all -d /dev/v4l-subdev12 > /home/root/digitalgain_check.log")
+
+    #verdict = false
+    with open('/home/root/digitalgain_check.log') as f:
+        if 'digital_gain 0x009f0905 (int)    : min=0 max=2047 step=2 default=128 value=128' in f.read():
+          print ("Digital Gain PASS")
+        else:
+          print ("Digital Gain FAILED")
+          
+def analog_gain():
+    print("----- Analog Gain Value Checking -----")
+    os.system("v4l2-ctl --all -d /dev/v4l-subdev12 > /home/root/analoggain_check.log")
+
+    #verdict = false
+    with open('/home/root/analoggain_check.log') as f:
+        if 'analogue_gain 0x009e0903 (int)    : min=0 max=127 step=2 default=14 value=14' in f.read():
+          print ("Analog Gain PASS")
+        else:
+          print ("Analog Gain FAILED")
+          
+          
 def main():
     print("----- IPU Module/Firmware Checking -----")
     script_name = str(sys.argv[0])
@@ -57,7 +89,13 @@ def main():
         driver_checker()
     elif args.c == "ipu_modules_checker":
         modules_checker()
-
+    elif args.c == "exposure":
+        exposure()
+    elif args.c == "digital_gain":
+        digital_gain()
+    elif args.c == "analog_gain":
+        analog_gain()
+   
     else:
         print("Invalid parameters !! ")
         sys.exit(0)
